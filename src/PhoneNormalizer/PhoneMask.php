4707e3febf00f7eae9bf1432f1cf85fd041187b4
Zhenya Sayapin <ep.sayapin@gmail.com>
2025-06-25 19:20:07 +0400

    Phone mask is fixed

--- src/PhoneNormalizer/PhoneMask.php
+++ src/PhoneNormalizer/PhoneMask.php
@@ -6,13 +6,13 @@ use App\Contracts\PhoneMaskInterface;
 
 class PhoneMask implements PhoneMaskInterface
 {
-    private const PATTERN = '/(\d{1})(\d{3})(\d{3})(\d{4})/';
+    private const PATTERN = '/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/';
 
     public function mask(string $phone): string
     {
         return preg_replace(
             self::PATTERN, 
-            '$1($2) $3-$4', 
+            '$1 ($2) $3 $4 $5',
             $phone
         );
     }
